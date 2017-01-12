a5f198543c282ec26a39ea16b629f010286ce387
Jailani Abdul Rahman <jailani.har@googlemail.com>
2017-01-12 09:02:04 +0800

    Implemented methods that return data and a constructor for Car.java

--- chp02/Car.java
+++ chp02/Car.java
@@ -8,16 +8,26 @@ public class Car {
 	int currentSpeed;
 	int totalMileage;
 	static int noOfCarCreated = 0;
-	
-	public void pressAccelerator(int speed) {
-		String msg = "Ku takan jua ni";
-		System.out.println(msg);
+
+	public Car(String colour, int noOfDoors, int gasMileage, int currentSpeed, int totalMileage) {
+		this.colour = colour;
+		this.noOfDoors = noOfDoors;
+		this.gasMileage = gasMileage;
+		this.currentSpeed = currentSpeed;
+		this.totalMileage = totalMileage;
+	}
+
+	public String pressAccelerator(int speed) {
 		currentSpeed += speed;
 		gasMileage -= 5;
 		totalMileage +=5;
+		String msg = "I already pressed the accelerator";
+		return msg;
 	}
 	
-	public void pressBrake() {
+	public String pressBrake() {
 		currentSpeed = 0;
+		String msg = "I already pressed the brake";
+		return msg;
 	}
 }
--- chp02/RunProgram.java
+++ chp02/RunProgram.java
@@ -3,42 +3,32 @@ package chp02;
 public class RunProgram {
 
 	public static void main(String[] args) {
-		Car firstCar = new Car();
-		firstCar.colour = "Blue";
-		firstCar.noOfDoors = 5;
-		firstCar.gasMileage = 500;
-		firstCar.currentSpeed = 0;
-		firstCar.totalMileage = 100;
+		Car firstCar = new Car("Blue", 5, 500, 0, 100);
 		
 		System.out.println("Initial speed of firstCar");
 		System.out.println(firstCar.currentSpeed);
 		
 		System.out.println("Pressing firstCar accelerator");
-		firstCar.pressAccelerator(20);
+		System.out.println(firstCar.pressAccelerator(20));;
 		System.out.println(firstCar.currentSpeed);
 		
 		System.out.println("Pressing firstCar accelerator");
-		firstCar.pressAccelerator(10);
+		System.out.println(firstCar.pressAccelerator(10));
 		System.out.println(firstCar.currentSpeed);
 		
 		System.out.println("Pressing firstCar brake");
-		firstCar.pressBrake();
+		System.out.println(firstCar.pressBrake());;
 		System.out.println("Current Speed: " + firstCar.currentSpeed);
 		System.out.println("Current Gas Mileage " + firstCar.gasMileage);
 		System.out.println("Current Total Mileage " + firstCar.totalMileage);
 		
-		Car secondCar = new Car();
-		secondCar.colour = "White";
-		secondCar.noOfDoors = 5;
-		secondCar.currentSpeed = 0;
-		secondCar.gasMileage = 200;
-		secondCar.totalMileage = 10000;
+		Car secondCar = new Car("White", 5, 0, 200, 10000);
 		
 		System.out.println("Initial speed of secondCar");
 		System.out.println(secondCar.currentSpeed);
 		
 		System.out.println("Pressing secondCar accelerator");
-		secondCar.pressAccelerator(30);
+		System.out.println(secondCar.pressAccelerator(30));;
 		
 		System.out.println("firstCar Speed " + firstCar.currentSpeed);
 		System.out.println("secondCar Speed " + secondCar.currentSpeed);
