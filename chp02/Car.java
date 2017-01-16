ecccadcf2963fa84c50fc05cef9323164ad9f494
Jailani Abdul Rahman <jailani.har@googlemail.com>
2017-01-16 10:12:36 +0800

    Implemented Constructor and Methods Overloading

--- chp02/Car.java
+++ chp02/Car.java
@@ -5,9 +5,17 @@ public class Car {
 	String colour;
 	int noOfDoors;
 	int gasMileage;
-	int currentSpeed;
+	public int currentSpeed;
 	int totalMileage;
 	static int noOfCarCreated = 0;
+
+	public Car() {
+		this("Blue", 5, 0, 0, 0);
+	}
+	
+	public Car(int gasMileage, int currentSpeed, int totalMileage) {
+		this("Blue", 5, gasMileage, currentSpeed, totalMileage);
+	}
 
 	public Car(String colour, int noOfDoors, int gasMileage, int currentSpeed, int totalMileage) {
 		this.colour = colour;
@@ -15,6 +23,10 @@ public class Car {
 		this.gasMileage = gasMileage;
 		this.currentSpeed = currentSpeed;
 		this.totalMileage = totalMileage;
+	}
+	
+	public String pressAccelerator() {
+		return this.pressAccelerator(10);
 	}
 
 	public String pressAccelerator(int speed) {
